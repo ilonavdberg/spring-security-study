e3ffaa2f387ea0b5c658188d795c574020480320
ilonavdberg <137798906+ilonavdberg@users.noreply.github.com>
2025-10-11 23:49:30 +0200

    initialized project with Docker PostgreSQL container

---
+++ src/test/java/dev/ilona/spring_security_study/ApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.ilona.spring_security_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
