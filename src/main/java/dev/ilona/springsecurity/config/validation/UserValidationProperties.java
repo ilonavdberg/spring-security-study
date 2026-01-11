ccf2215fa0f86efe04269566c192c2b4f925cc9b
ilonavdberg <137798906+ilonavdberg@users.noreply.github.com>
2026-01-11 14:41:37 +0100

    fix bug related to custom type FieldValidation

--- src/main/java/dev/ilona/springsecurity/config/validation/UserValidationProperties.java
+++ src/main/java/dev/ilona/springsecurity/config/validation/UserValidationProperties.java
@@ -1,16 +1,18 @@
 package dev.ilona.springsecurity.config.validation;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "validation.user")
-@AllArgsConstructor
+@NoArgsConstructor
+@Setter
 @Getter
 public class UserValidationProperties {
-    private final FieldValidation username;
-    private final FieldValidation password;
-    private final FieldValidation email;
+    private FieldValidation username;
+    private FieldValidation password;
+    private FieldValidation email;
 }
