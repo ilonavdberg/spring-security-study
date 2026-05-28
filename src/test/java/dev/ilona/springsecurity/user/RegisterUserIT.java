a0962d692ed6443c431e312dc7c046205307cbcc
ilonavdberg <137798906+ilonavdberg@users.noreply.github.com>
2026-05-28 16:31:50 +0200

    test exception flows for user registration

--- src/test/java/dev/ilona/springsecurity/user/RegisterUserIT.java
+++ src/test/java/dev/ilona/springsecurity/user/RegisterUserIT.java
@@ -7,20 +7,25 @@ import dev.ilona.springsecurity.common.TestDataInitializer;
 import dev.ilona.springsecurity.domain.user.User;
 import dev.ilona.springsecurity.domain.user.UserRepository;
 import dev.ilona.springsecurity.domain.user.role.Role;
+import dev.ilona.springsecurity.exception.exceptions.DuplicateEntryException;
+import dev.ilona.springsecurity.exception.exceptions.PolicyViolationException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ActiveProfiles("test")
 @SpringBootTest
-@AutoConfigureMockMvc
+@Transactional
 @Import({
         PostgresTestContainerConfig.class,
         TestDataInitializer.class
@@ -33,12 +38,29 @@ public class RegisterUserIT {
     @Autowired
     UserRepository userRepository;
 
+    @Value("${application.email.domain}")
+    private String internalEmailDomain;
+
+    private final String duplicateUsername = "duplicate-user";
+    private final String duplicateEmail = "duplicate@email.com";
+
+    @BeforeEach
+    void setup() {
+        UserRegistrationRequest request = new UserRegistrationRequest(
+                duplicateUsername,
+                duplicateEmail,
+                "P@ssW0rd"
+        );
+
+        userManagementService.registerUser(request);
+    }
+
     @Test
     void shouldRegisterUserSuccessfully() {
         UserRegistrationRequest request = new UserRegistrationRequest(
                 "test-user",
                 "test-user@email.com",
-                "P@ssw0rd"
+                "P@ssW0rd"
         );
 
         UUID uuid = userManagementService.registerUser(request);
@@ -59,6 +81,53 @@ public class RegisterUserIT {
             assertThat(user.isBlocked()).isFalse();
             assertThat(user.isDeleted()).isFalse();
         });
+    }
+
+    @Test
+    void shouldNotRegisterUserWhenUsernameAlreadyExists() {
+        String email = "test@email.com";
+        UserRegistrationRequest request = new UserRegistrationRequest(
+                duplicateUsername,
+                email,
+                "P@ssW0rd"
+        );
+
+        assertThatThrownBy(() -> userManagementService.registerUser(request))
+                .isInstanceOf(DuplicateEntryException.class)
+                .hasMessageMatching("(?i).*username.*");
+
+        assertThat(userRepository.existsByEmail(email)).isFalse();
+    }
+
+    @Test
+    void shouldNotRegisterUserWhenEmailAlreadyExists() {
+        String username = "username";
+        UserRegistrationRequest request = new UserRegistrationRequest(
+                username,
+                duplicateEmail,
+                "P@ssW0rd"
+        );
+
+        assertThatThrownBy(() -> userManagementService.registerUser(request))
+                .isInstanceOf(DuplicateEntryException.class)
+                .hasMessageMatching("(?i).*email.*");
+
+        assertThat(userRepository.existsByUsername(username)).isFalse();
+    }
+
+    @Test
+    void shouldNotRegisterUserWithInternalEmailDomain() {
+        String username = "username";
+        UserRegistrationRequest request = new UserRegistrationRequest(
+                "username",
+                "email@" + internalEmailDomain,
+                "P@ssW0rd"
+        );
+
+        assertThatThrownBy(() -> userManagementService.registerUser(request))
+                .isInstanceOf(PolicyViolationException.class);
+
+        assertThat(userRepository.existsByUsername(username)).isFalse();
     }
 
 }
